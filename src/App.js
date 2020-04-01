e5b3717ef66c63bc9bcf35075c7301584ff24bea
Kapil-pandey <kapil.pandey@driscolls.com>
2020-04-01 20:51:39 +0530

    added material-ui skeleton with redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from './theme'
 import { connect } from "react-redux";
 import './App.css';
-
-class App extends React.Component{
-  render(){
-    return(
+class App extends React.Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
       <div>
-        <p>Hello</p>
+        <p>Added Material theme!</p>
       </div>
+    </ThemeProvider>
     )
   }
 }
