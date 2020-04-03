05d0947123afe5901f931fa08fe8cf914e4d77d9
Kapil <abyss6271@gmail.com>
2020-04-04 00:05:42 +0530

    Route logic corrected

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from "react-redux";
 import Search from './components/not used/Search'
 import IndiaView from './components/IndiaView'
+import CityView from './components/CityView'
 import PersistentDrawerRight from './components/drawer'
 import './App.css';
 import TableView from './components/TableView'
@@ -22,9 +23,11 @@ class App extends React.Component {
       <Router history={Router.browserHistory}>
       <PersistentDrawerRight/>
        <Switch>
-        <Route  path="/" component={IndiaView} />
         {/* <Route exact path="/IndiaView" component={IndiaView} /> */}
         <Route exact path="/All" component={TableView} />
+        <Route  exact path="/India" component={IndiaView} />
+        <Route  exact path="/" component={IndiaView} />
+        <Route exact path="/State/:name" component={CityView} />
       </Switch>
       </Router>,
       {/* </PersistentDrawerRight> */}
