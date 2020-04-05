dd8c1b24eadd0627af749fc0f0002d55899c6fc8
Kapil <abyss6271@gmail.com>
2020-04-05 12:55:24 +0530

    Added Resources page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import IndiaView from './components/IndiaView'
 import IndiaView2 from './components/IndiaView2'
+import Resources from './components/About'
 import CityView from './components/CityView'
 import PersistentDrawerRight from './components/drawer'
 import CountryView from './components/CountryView'
@@ -23,6 +24,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/All" component={CountryView} />
             <Route exact path="/India" component={IndiaView2} />
+            <Route exact path="/Resources" component={Resources} />
             <Route exact path="/" component={IndiaView2} />
             <Route exact path="/State/:name" component={CityView} />
           </Switch>
