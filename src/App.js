3ace54e1b8422f675b14b9e7759fc06d9ba0671d
Kapil <abyss6271@gmail.com>
2020-04-05 11:53:04 +0530

    Removed unwanted dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { connect } from "react-redux";
 import IndiaView from './components/IndiaView'
+import IndiaView2 from './components/IndiaView2'
 import CityView from './components/CityView'
 import PersistentDrawerRight from './components/drawer'
-import './App.css';
 import CountryView from './components/CountryView'
 import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
@@ -11,24 +10,24 @@ import theme from './theme'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 const styles = {
   paperContainer: {
-      // backgroundImage: `url(${Image})`
+    // backgroundImage: `url(${Image})`
   }
 };
 class App extends React.Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router history={Router.browserHistory}>
-      <PersistentDrawerRight/>
-       <Switch>
-        <Route exact path="/All" component={CountryView} />
-        <Route  exact path="/India" component={IndiaView} />
-        <Route  exact path="/" component={IndiaView} />
-        <Route exact path="/State/:name" component={CityView} />
-      </Switch>
-      </Router>
-    </ThemeProvider>
+        <CssBaseline />
+        <Router history={Router.browserHistory}>
+          <PersistentDrawerRight />
+          <Switch>
+            <Route exact path="/All" component={CountryView} />
+            <Route exact path="/India" component={IndiaView} />
+            <Route exact path="/" component={IndiaView2} />
+            <Route exact path="/State/:name" component={CityView} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
     )
   }
 }
