1cba6f8ceef8ee6eeb72a6b56cafb85e9c280910
kp <abyss6271@gmail.com>
2020-04-12 08:17:35 +0530

    Timeline working with date filter

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -3,7 +3,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import { connect } from 'react-redux';
 import axios from 'axios';
-import { setCaseSeries, setLoader,hideMessage,showMessage } from '../actions/index'
+import { setCaseSeries, setLoader, hideMessage, showMessage } from '../actions/index'
 import LineChart from '../components/LineChart'
 const styles = (theme) => ({
   root: {
@@ -17,71 +17,79 @@ const styles = (theme) => ({
 
 class TimeSeries extends React.Component {
   state = {
-    labels: [],
-    data: []
+    caseSeries:[]
   }
-  makeGraphData = (arr) => {
+  makeGraphData = (originalArr) => {
+    let arr = originalArr.filter(itm => {
+      let dt = new Date(itm.date + 2020).getTime()
+      return (dt >= new Date(this.props.startDate).getTime()) && (dt <= new Date(this.props.endDate).getTime())
+    })
     let labels = [],
       dailyconfirmed = [],
       dailydeceased = [],
       dailyrecovered = []
+      this.props.setLoader(false)
     arr.map(itm => {
       labels.push(itm.date)
       dailyconfirmed.push(+itm.dailyconfirmed)
       dailydeceased.push(+itm.dailydeceased)
       dailyrecovered.push(+itm.dailyrecovered)
     })
-    this.setState({
+    return({
       labels,
-      data: 
-      [
-        {
-          label: "Deceased",          
-          backgroundColor: '#008B8E',
-          borderColor: "#008B8E",
-          data: dailydeceased,
-          borderWidth: 1,
-          fill: false,
-          spanGaps: true,
-        },
-        {
-          label: 'Recovered',
-          backgroundColor: 'green',
-          data: dailyrecovered,
-          borderColor: "green",
-          borderWidth: 1,
-          fill: false, spanGaps: true,
-        }, {
-         label: 'New Cases',
-          backgroundColor: "rgba(160,0,0,0.4)",
-          borderColor: "#9F0000",
-          data: dailyconfirmed,
-          borderWidth: 1,
-          fill: false, spanGaps: true,
-        },
-      ]
-    }, () => this.props.setLoader(false))
+      data:
+        [
+          {
+            label: "Deceased",
+            backgroundColor: '#008B8E',
+            borderColor: "#008B8E",
+            data: dailydeceased,
+            borderWidth: 1,
+            fill: false,
+            spanGaps: true,
+          },
+          {
+            label: 'Recovered',
+            backgroundColor: 'green',
+            data: dailyrecovered,
+            borderColor: "green",
+            borderWidth: 1,
+            fill: false, spanGaps: true,
+          }, {
+            label: 'New Cases',
+            backgroundColor: "rgba(160,0,0,0.4)",
+            borderColor: "#9F0000",
+            data: dailyconfirmed,
+            borderWidth: 1,
+            fill: false, spanGaps: true,
+          },
+        ]
+    })
   }
   async componentDidMount() {
     this.props.setLoader(true)
     if (!this.props.series.length) {
-      this.props.showMessage("info","Fetching data")
+      this.props.showMessage("info", "Fetching data")
       await axios.get(`https://api.covid19india.org/data.json`)
         .then(res => {
           let response = res.data
+          this.setState({caseSeries:response.cases_time_series})
           this.props.setCaseSeries(response.cases_time_series)
-          this.makeGraphData(response.cases_time_series)
-        }).catch(e =>this.props.showMessage("error","An error occured! \n Please try again"))
-    } else
-      this.makeGraphData(this.props.series)
+          this.props.showMessage("success", "You can toggle between 3 option")
+        }).catch(e => this.props.showMessage("error", "An error occured! \n Please try again"))
+    } else{
+      this.setState({caseSeries:this.props.series})
+      this.props.setCaseSeries(this.props.series)
+  }
   }
   render() {
+    let graphObj=this.makeGraphData(this.props.series)
     const { classes } = this.props
     return (
       <Paper className={classes.root}>
         <LineChart
-          datasets={this.state.data}
-          labels={this.state.labels}
+          datasets={graphObj.data}
+          labels={graphObj.labels}
           text={`Click on any label`}
         />
       </Paper>
@@ -90,13 +98,15 @@ class TimeSeries extends React.Component {
 }
 const mapDispatchToProps = (dispatch) => ({
   setCaseSeries: (data) => dispatch(setCaseSeries(data)),
-  setLoader: (data) => dispatch(setLoader(data)),  
-  showMessage: (type,msg) => dispatch(showMessage(type,msg)),
+  setLoader: (data) => dispatch(setLoader(data)),
+  showMessage: (type, msg) => dispatch(showMessage(type, msg)),
   hideMessage: (data) => dispatch(hideMessage(data)),
 });
 function mapStateToProps(state) {
   return {
-    series: state.rootReducer.caseSeries
+    series: state.rootReducer.caseSeries,
+    startDate: state.dateSearch.startDate,
+    endDate: state.dateSearch.endDate,
   }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(TimeSeries))
