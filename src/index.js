73263e9124d7f77c8b5e36eb86fb538f077d9afe
kp <abyss6271@gmail.com>
2020-04-12 08:54:21 +0530

    included dev depende..

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Store } from './store';
+import DateFnsUtils from '@date-io/date-fns';
 import App from './App';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={Store}>
-    <App />
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <App />
+    </MuiPickersUtilsProvider>
   </Provider>,
   document.getElementById('root')
 );
